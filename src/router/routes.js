2f0aba1aece81a66072b9bd6abe9f6bf72e6de25
Salman Khan <khansalman.ks1998@gmail.com>
2024-12-30 22:50:10 +0530

    deployment test

--- src/router/routes.js
+++ src/router/routes.js
@@ -10,7 +10,7 @@ const routes = [
   },
 
   // Always leave this as last one,
-  // but you can also remove it
+  // but you can also remove ith
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
